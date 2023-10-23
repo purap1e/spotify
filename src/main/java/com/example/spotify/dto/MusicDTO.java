e74af046d2c9874cd765d8d7495c483487dbe977
Daniyar Bulegenov <purap1e@mail.ru>
2023-10-23 20:45:06 +0600

    refactor1

--- src/main/java/com/example/spotify/dto/MusicDTO.java
+++ src/main/java/com/example/spotify/dto/MusicDTO.java
@@ -4,5 +4,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-public record MusicDTO(UUID id, String name, List<SingerDTO> singers, Date dateOfCreation, String imageUrl, String audioUrl, List<String> genres) {
+public record MusicDTO(UUID id, String name, List<SingerDTO> singers, Date dateOfCreation, String imageUrl, String audioUrl,
+                       List<String> genres) {
 }
